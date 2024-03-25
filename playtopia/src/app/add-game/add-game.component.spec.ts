bdbc47665e601f82e0ce975b9055fb9835474471
TashevYanislav <tashevyanislav@gmail.com>
2024-03-25 11:42:20 +0200

    Created Add game Component

---
+++ playtopia/src/app/add-game/add-game.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddGameComponent } from './add-game.component';
+
+describe('AddGameComponent', () => {
+  let component: AddGameComponent;
+  let fixture: ComponentFixture<AddGameComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddGameComponent]
+    });
+    fixture = TestBed.createComponent(AddGameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
