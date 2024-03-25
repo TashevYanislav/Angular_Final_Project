ed02f89d310e417302cb71b87b5824073b956216
TashevYanislav <tashevyanislav@gmail.com>
2024-03-25 11:10:35 +0200

    Created Core module

--- playtopia/src/app/app.module.ts
+++ playtopia/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoreComponent
-  ],
+    AppComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule
