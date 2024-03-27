ea4d868e1d165f4a79ce96ec798c04eb0b4414b8
TashevYanislav <tashevyanislav@gmail.com>
2024-03-27 16:36:53 +0200

    Update app-routing.module.ts

--- playtopia/src/app/app-routing.module.ts
+++ playtopia/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full" , redirectTo: "/"},
   { path: '', component: MainComponent },
   { path: 'about', component: AboutComponent },
-  // {path:"**",redirectTo"/404"},
-  // {path:"404",component:ErrorComponent}
+  {path:"**",redirectTo:"/404"},
+  {path:"404",component:ErrorComponent}
 ];
 
 @NgModule({
