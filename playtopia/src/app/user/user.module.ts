b139bee491a62e85fb671a0a19591c2eaf7a8385
TashevYanislav <tashevyanislav@gmail.com>
2024-03-27 12:44:11 +0200

    User component and user routing function created

---
+++ playtopia/src/app/user/user.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserRoutingModule } from './user-router.module';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, UserRoutingModule],
+  exports: [LoginComponent, RegisterComponent],
+})
+export class UserModule {}
