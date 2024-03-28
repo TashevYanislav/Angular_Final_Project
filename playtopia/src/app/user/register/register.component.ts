a8f45f62d1b28ce5892387a2e39070bf95288ade
TashevYanislav <tashevyanislav@gmail.com>
2024-03-28 12:08:45 +0200

    Add register Function

--- playtopia/src/app/user/register/register.component.ts
+++ playtopia/src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,18 +8,43 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private http: HttpClient) {}
+
   formSubmitHandler(form: NgForm) {
-    const { username, email, password, repassword } = form?.value;
+    const { username, email, password, repassword } = form.value;
+    const URL: string = 'http://localhost:3030/users/register';
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+      repassword: repassword,
+    };
+
+    if (password !== repassword) {
+      console.log("Passwords don't match");
+      return;
+    }
 
     if (form.invalid) {
       console.log('Form Invalid');
 
       return;
     }
-    console.log(
-      `username: ${username}  email: ${email}  password: ${password}  repassword: ${repassword}`
-    );
+    console.log(form.value);
 
-    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    this.http.post(URL, data, httpOptions).subscribe(
+      (response) => {
+        console.log('POST request was successful', response);
+      },
+      (error) => {
+        console.error('Error occurred during POST request', error);
+      }
+    );
   }
 }
--- playtopia/src/app/user/user.module.ts
+++ playtopia/src/app/user/user.module.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-router.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, UserRoutingModule,FormsModule],
+  imports: [CommonModule, UserRoutingModule,FormsModule,HttpClientModule ],
   exports: [LoginComponent, RegisterComponent],
 })
 export class UserModule {}
