3b9282f243a26a1194ac545b1d8810e5bee23128
TashevYanislav <tashevyanislav@gmail.com>
2024-03-29 10:46:40 +0200

    Update user.ts

--- playtopia/src/app/types/user.ts
+++ playtopia/src/app/types/user.ts
@@ -1,5 +1,10 @@
-export interface UserData {
+export interface RegisterData {
   username: string;
   email: string;
+  password: string;
+}
+
+export interface LoginData {
+  email:string;
   password: string;
 }
