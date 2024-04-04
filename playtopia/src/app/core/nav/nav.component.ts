8385672d05bdbf6b76364a11f84bf340bd4d65a4
TashevYanislav <tashevyanislav@gmail.com>
2024-04-04 14:29:05 +0300

    Create Logged Nav Component

--- playtopia/src/app/core/nav/nav.component.ts
+++ playtopia/src/app/core/nav/nav.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
+  constructor(public userService: UserService) {}
 
+  logoutHandler() {
+    this.userService.logout();
+  }
 }
