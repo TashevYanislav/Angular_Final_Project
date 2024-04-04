3205d2dc160fe52028488d207766ff99ee6dc7ea
TashevYanislav <tashevyanislav@gmail.com>
2024-04-04 21:01:14 +0300

    Main renders the lates games

--- playtopia/src/app/main/main.component.ts
+++ playtopia/src/app/main/main.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game/game.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  latestGames: any[] = [];
 
+  constructor(private gameService: GameService) {}
+
+  ngOnInit(): void {
+    this.gameService.getLatestFiveGames().subscribe((data: any) => {
+      this.latestGames = data;
+    });
+  }
 }
