9454fb416f4237efd7f78b817dc17c23e43e27bc
TashevYanislav <tashevyanislav@gmail.com>
2024-04-09 10:57:39 +0300

    implemented delete like func

--- playtopia/src/app/game/detail/detail.component.ts
+++ playtopia/src/app/game/detail/detail.component.ts
@@ -11,6 +11,8 @@ export class DetailComponent implements OnInit {
   game: any = null;
   user_id = localStorage.getItem('user_id');
   id: string | null = this.activeRoute.snapshot.paramMap.get('id');
+  isLiked: boolean = false;
+  like_id: string | null = '';
   constructor(
     public gameService: GameService,
     private activeRoute: ActivatedRoute,
@@ -28,6 +30,18 @@ export class DetailComponent implements OnInit {
     this.router.navigate(['/store']);
   }
   LikeHandler() {
-    this.gameService.likeGame(this.id,this.user_id).subscribe(console.log);
+    if (!this.isLiked) {
+      this.gameService
+        .likeGame(this.id, this.user_id)
+        .subscribe((response: any) => {
+          this.like_id = response._id;
+        });
+      this.isLiked = !this.isLiked;
+    } else {
+      console.log(this.id, this.like_id);
+
+      this.gameService.delteLikeGame(this.like_id).subscribe();
+      this.isLiked = !this.isLiked;
+    }
   }
 }
