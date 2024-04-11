c70f5addc13dfd528ce54681bb09019ca4bdeae5
TashevYanislav <tashevyanislav@gmail.com>
2024-04-12 00:05:08 +0300

    Update game-routing.module.ts

--- playtopia/src/app/game/game-routing.module.ts
+++ playtopia/src/app/game/game-routing.module.ts
@@ -4,12 +4,14 @@ import { StoreComponent } from './store/store.component';
 import { AddGameComponent } from './add-game/add-game.component';
 import { DetailComponent } from './detail/detail.component';
 import { EditComponent } from './edit/edit.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: 'store', component: StoreComponent },
   { path: 'add_game', component: AddGameComponent },
   { path: 'details/:id', component: DetailComponent },
   { path: 'edit/:id', component: EditComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
