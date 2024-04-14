d329f33bf3924277aa5da3bfb1f76e3a331c8e2d
TashevYanislav <tashevyanislav@gmail.com>
2024-04-14 20:39:11 +0300

    Implemented cart irem rendering

--- playtopia/src/app/game/cart/cart.component.ts
+++ playtopia/src/app/game/cart/cart.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  user_id: string | null = localStorage.getItem('user_id');
+  cartGames: any[] = [];
+  constructor(private gameService: GameService) {}
 
+  ngOnInit(): void {
+    this.gameService.getAllGamesByUser(this.user_id).subscribe(
+      (data: any) => {
+        this.cartGames = data;
+      },
+      (error) => {
+        console.error('Error occurred while fetching games:', error);
+      }
+    );
+  }
 }
