d9980f565839345587ea9c870a80ee3943395b1e
TashevYanislav <tashevyanislav@gmail.com>
2024-04-21 19:35:26 +0300

    Created onDeleteHandler func

--- playtopia/src/app/game/cart/cart.component.ts
+++ playtopia/src/app/game/cart/cart.component.ts
@@ -9,16 +9,44 @@ import { GameService } from '../game.service';
 export class CartComponent implements OnInit {
   user_id: string | null = localStorage.getItem('user_id');
   cartGames: any[] = [];
+  totalPrice: number = 0; // Initialize totalPrice
   constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
     this.gameService.getAllGamesByUser(this.user_id).subscribe(
       (data: any) => {
         this.cartGames = data;
+        console.log(data);
+        // Calculate total price after fetching data
+        this.calculateTotalPrice();
       },
       (error) => {
         console.error('Error occurred while fetching games:', error);
       }
+    );
+  }
+
+  onDeleteHandler(id: string) {
+    console.log('Clicked');
+    this.gameService.deleteCartGame(id, this.user_id).subscribe(() => {
+      this.gameService.getAllGamesByUser(this.user_id).subscribe(
+        (data: any) => {
+          this.cartGames = data;
+          console.log(data);
+          // Calculate total price after fetching data
+          this.calculateTotalPrice();
+        },
+        (error) => {
+          console.error('Error occurred while fetching games:', error);
+        }
+      );
+    });
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = this.cartGames.reduce(
+      (total, game) => total + game.gamePrice,
+      0
     );
   }
 }
