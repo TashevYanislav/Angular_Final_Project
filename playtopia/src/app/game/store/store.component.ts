5eec93e7397cf55a36bceaa18e1c24dcaa47b3e6
TashevYanislav <tashevyanislav@gmail.com>
2024-04-22 14:25:26 +0300

    Loader Func

--- playtopia/src/app/game/store/store.component.ts
+++ playtopia/src/app/game/store/store.component.ts
@@ -8,48 +8,61 @@ import { GameService } from '../game.service';
 })
 export class StoreComponent implements OnInit {
   games: any[] = [];
+  isLoaded: boolean = false; // Initialize isLoaded variable to false
+
   constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
-    this.gameService.getAllGames().subscribe(
-      (data: any) => {
-        this.games = data;
-      },
-      (error) => {
-        console.error('Error occurred while fetching games:', error);
-      }
-    );
+    this.loadData();
   }
-  getAllHandler() {
+
+  loadData() {
+    this.games = [];
+    this.isLoaded = false; // Set isLoaded to false before loading data
     this.gameService.getAllGames().subscribe(
       (data: any) => {
         this.games = data;
+        this.isLoaded = true; // Update isLoaded to true after data is loaded
       },
       (error) => {
         console.error('Error occurred while fetching games:', error);
+        this.isLoaded = true; // Ensure isLoaded is set to true even on error
       }
     );
   }
 
+  getAllHandler() {
+    this.loadData(); // Reuse loadData method
+  }
+
   getGameByGenreHandler(genre: string) {
-    this.games = []; // Clear the games array
+    this.games = [];
+    this.isLoaded = false; // Set isLoaded to false before loading data
     this.gameService.getGamesByGenre(genre).subscribe(
       (data: any) => {
         this.games = data;
+        // Assign data to this.games first
+        this.isLoaded = true; // Then set isLoaded to true after data is loaded
       },
       (error) => {
         console.error('Error occurred while fetching games by genre:', error);
+        this.isLoaded = true; // Ensure isLoaded is set to true even on error
       }
     );
   }
 
   searchHandler(searchParams: string | null) {
+    this.games = [];
+    this.isLoaded = false; // Set isLoaded to false before loading data
     this.gameService.searchGame(searchParams).subscribe(
       (data: any) => {
         this.games = data;
+        // Assign data to this.games first
+        this.isLoaded = true; // Then set isLoaded to true after data is loaded
       },
       (error) => {
         console.error('Error occurred while fetching games by genre:', error);
+        this.isLoaded = true; // Ensure isLoaded is set to true even on error
       }
     );
   }
