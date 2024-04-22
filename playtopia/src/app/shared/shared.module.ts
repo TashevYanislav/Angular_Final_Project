83ebf1810a04d1744ef63296330956a9757f7a23
TashevYanislav <tashevyanislav@gmail.com>
2024-04-22 14:25:01 +0300

    Update shared.module.ts

--- playtopia/src/app/shared/shared.module.ts
+++ playtopia/src/app/shared/shared.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { GamesRowComponent } from './games-row/games-row.component';
 import { RouterModule } from '@angular/router';
 import { LoaderComponent } from './loader/loader.component';
+import { PopularGamesLoaderComponent } from './popular-games-loader/popular-games-loader.component';
 
 @NgModule({
-  declarations: [GamesRowComponent, LoaderComponent],
+  declarations: [
+    GamesRowComponent,
+    LoaderComponent,
+    PopularGamesLoaderComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [GamesRowComponent],
+  exports: [GamesRowComponent, PopularGamesLoaderComponent],
 })
 export class SharedModule {}
