1770b0bd08eabf06927610cb1495345831b4a768
TashevYanislav <tashevyanislav@gmail.com>
2024-04-22 14:25:34 +0300

    Displaying the loader

--- playtopia/src/app/main/main.component.ts
+++ playtopia/src/app/main/main.component.ts
@@ -10,12 +10,17 @@ import { forkJoin } from 'rxjs';
 export class MainComponent implements OnInit {
   latestGames: any[] = [];
   popularGames: any[] = [];
+  isLoaded: boolean = false;
+  isLoadedPupolarGames: boolean = false;
 
   constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
+    this.isLoaded = false;
+    this.isLoadedPupolarGames = false;
     this.gameService.getLatestFiveGames().subscribe((data: any) => {
       this.latestGames = data;
+      this.isLoaded = true;
     });
     this.gameService.getMostPopular(3).subscribe((topGamesObject) => {
       const gameIds = Object.keys(topGamesObject);
@@ -25,6 +30,7 @@ export class MainComponent implements OnInit {
       forkJoin(observables).subscribe((gamesData: any[]) => {
         this.popularGames = gamesData;
         console.log('Popular popularGames', this.popularGames);
+        this.isLoadedPupolarGames = true;
       });
     });
   }
