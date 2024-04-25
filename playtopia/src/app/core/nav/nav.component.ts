68bb63ce9c347c2288eeaee3f6fabd4e0b5c061f
TashevYanislav <tashevyanislav@gmail.com>
2024-04-25 13:25:26 +0300

    Implemented getCartGamesCount

--- playtopia/src/app/core/nav/nav.component.ts
+++ playtopia/src/app/core/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/game/game.service';
 import { UserService } from 'src/app/user/user.service';
 
 @Component({
@@ -6,8 +7,20 @@ import { UserService } from 'src/app/user/user.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-  constructor(public userService: UserService) {}
+export class NavComponent implements OnInit {
+  user_id: string | null = localStorage.getItem('user_id');
+  cartGamesCount: number = 0;
+  constructor(
+    public userService: UserService,
+    public gameService: GameService
+  ) {}
+
+  ngOnInit(): void {
+    this.gameService.getCartGamesCount(this.user_id).subscribe((data) => {
+      this.cartGamesCount = data;
+      console.log(`CART GAMES: ${this.cartGamesCount}`);
+    });
+  }
 
   logoutHandler() {
     this.userService.logout();
