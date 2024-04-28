8fad79697584b9ee62ce3e5065fef6a862e59efb
TashevYanislav <tashevyanislav@gmail.com>
2024-04-28 12:47:27 +0300

    Update util.js

--- softuni-practice-server-master/src/services/util.js
+++ softuni-practice-server-master/src/services/util.js
@@ -2,24 +2,23 @@
  * This service requires util plugin
  */
 
-const Service = require('./Service');
-
+const Service = require("./Service");
 
 const utilService = new Service();
 
-utilService.post('*', onRequest);
-utilService.get(':service', getStatus);
+utilService.post("*", onRequest);
+utilService.get(":service", getStatus);
 
 function getStatus(context, tokens, query, body) {
-    return context.util[context.params.service];
+  return context.util[context.params.service];
 }
 
 function onRequest(context, tokens, query, body) {
-    Object.entries(body).forEach(([k,v]) => {
-        console.log(`${k} ${v ? 'enabled' : 'disabled'}`);
-        context.util[k] = v;
-    });
-    return '';
+  Object.entries(body).forEach(([k, v]) => {
+    console.log(`${k} ${v ? "enabled" : "disabled"}`);
+    context.util[k] = v;
+  });
+  return "";
 }
 
 module.exports = utilService.parseRequest;
